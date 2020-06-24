ed366f07d5fbe91fdb56179e8979b31d7eb4e3ad
Lily24678 <lishangyun_lsy@163.com>
2020-06-24 16:33:47 +0800

    demo1

--- src/main/java/com/lsy/code/search/word/Demo1.java
+++ src/main/java/com/lsy/code/search/word/Demo1.java
@@ -1,9 +1,15 @@
 package com.lsy.code.search.word;
 
+import java.util.List;
+
+import org.apdplat.word.WordSegmenter;
+import org.apdplat.word.segmentation.Word;
+
 public class Demo1 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		List<Word> words = WordSegmenter.segWithStopWords("杨尚川是APDPlat应用级产品开发平台的作者");
+        System.out.println(words);
 
 	}
 
